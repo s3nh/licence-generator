11b5ac414763cde8f9a810934b440bf31d36f3b8
s3nh <steam.panek@gmail.com>
2019-11-17 06:18:57 -0500

    Added imag epreprocessing

--- build/lib/src/image_augmentation.py
+++ build/lib/src/image_augmentation.py
@@ -1,6 +1,10 @@
+import os 
 import Automold.Automold as am 
 import Automold.Helpers as hp
 
+os.makedirs('rain', exist_ok=True)
+os.makedirs('darken', exist_ok=True)
+os.makedirs('lighten', exist_ok=True)
 IMAGE_PATH = 'image/*.png'
 
 def load_images():
@@ -11,6 +15,7 @@ def main():
     _images = load_images()
     print(_images)
     print(IMAGE_PATH)
+    bright_images = am.brighten(_images[0:3])
+    hp.visualize(bright_images, column=3)
 if __name__ == "__main__":
     main()
-
